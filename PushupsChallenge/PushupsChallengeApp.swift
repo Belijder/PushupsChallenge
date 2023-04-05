c1321d928c5141082821ed4cc202e875ec07d9f9
Jakub Zajda <jakubzajda@gmail.com>
2023-04-05 11:50:03 +0200

    Initial Commit

---
+++ PushupsChallenge/PushupsChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PushupsChallengeApp.swift
+//  PushupsChallenge
+//
+//  Created by Jakub Zajda on 05/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct PushupsChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
